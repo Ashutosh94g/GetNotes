206be6740258c8df62325b953678972421e1d112
Ashutosh94g <46219709+Ashutosh94g@users.noreply.github.com>
2019-06-17 23:41:50 +0530

    Add files via upload

---
+++ GetNotes/GetNotes/views.py
@@ -0,0 +1,15 @@
+from django.shortcut import render , get_object_or_404
+
+def home_page(request):
+	template_name = 'home.html'
+	return render(request,template_name)
+
+
+def about_page(request):
+	template_name = 'base.html'
+	return render(request,template_name)
+
+
+def contect_page(request):
+	template_name = 'contect.html'
+	return render(request,template_name)
