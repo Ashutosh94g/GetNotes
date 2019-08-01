6556ee753ef16dcf6cf192b2db5b1d02a56c464d
Guneet Thind <guneethind@github.com>
2019-08-01 19:34:45 +0530

    g laptop running

--- GetNotes/GetNotes/GetNotes/urls.py
+++ GetNotes/GetNotes/GetNotes/urls.py
@@ -36,3 +36,5 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- GetNotes/notes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Notes
-
-
-admin.site.register(Notes)
